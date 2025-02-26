85a4f973527029050a1f1ec8c553fc366879a4ae
Aaditya <aadityaaryan639@gmail.com>
2025-02-26 20:08:18 +0530

    wrap the database operations in try-except blocks and use the new execute_query method.

--- init/initialize.py
+++ init/initialize.py
@@ -3,20 +3,18 @@ from database.db import Database
 from data.table_names import IMAGE_EMBEDDINGS_TABLE
 
 def initialize_db():
-
-    db = Database.get_instance().get_connection()
-    with db.cursor() as cur:
-        # Create images table if it does not exist
-        cur.execute(f"""
+    db = Database.get_instance()
+    try:
+        db.execute_query(f"""
             CREATE TABLE IF NOT EXISTS {IMAGE_EMBEDDINGS_TABLE} (
             id SERIAL PRIMARY KEY,
             embedding VECTOR(512),  -- Adjust this dimension based on the embedding size
             image_path TEXT NOT NULL
         );
         """)
-    
-        
-    print("Database initialized successfully.")
+        print("Database initialized successfully.")
+    except Exception as e:
+        print(f"Failed to initialize database: {e}")
 
 if __name__ == '__main__':
     initialize_db()
