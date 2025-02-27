a633fa67fd277b420233aa2dfb2a726d67edab26
Aadik1ng <61167519+Aadik1ng@users.noreply.github.com>
2025-02-27 13:14:27 +0530

    Delete data/env.py

--- data/env.py
+++
@@ -1,9 +0,0 @@
-# data/env.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # Loads variables from .env file if present
-
-POSTGRES_URL = os.getenv("POSTGRES_URL", "postgresql://aaditya:Hxjrhao22abjbkal1Qjz@194.195.112.175:5432/face_rec_1")
-CLIP_MODEL_PATH = os.getenv("CLIP_MODEL_PATH", "ViT-B/32")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "face_rec_1")
